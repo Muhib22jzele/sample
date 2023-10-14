6e48e9c0f45331bc8d311acd2f34077190cd4fed
Muhib22jzele <147899903+Muhib22jzele@users.noreply.github.com>
2023-10-13 21:57:04 -0700

    Add files via upload

---
+++ temp-1.py
@@ -0,0 +1,9 @@
+class toyota:
+    def __init__(self,color,model,):
+        self.color=color
+        self.model=model
+        def mov(self,x,y):
+            self.x=x
+            self.y=y
+obj1=toyota("Red",2023)
+print(obj1.color)
